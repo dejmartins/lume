f0c6496fc9e3553f46416c7036c1e6d75c45aab0
Deji Martins <dejimartins99@gmail.com>
2024-07-03 20:30:42 +0100

    portfolio performance added

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -4,3 +4,37 @@ export const formatCurrency = (amount: number) => {
         currency: 'NGN',
     });
 };
+
+export function getLabels(timeFrame: string) {
+    switch (timeFrame) {
+        case '24H':
+            return ['00:00', '06:00', '12:00', '18:00', '24:00'];
+        case '1W':
+            return ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+        case '1M':
+            return ['Week 1', 'Week 2', 'Week 3', 'Week 4'];
+        case '1Y':
+            return ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        case 'ALL':
+            return ['2018', '2019', '2020', '2021', '2022', '2023'];
+        default:
+            return [];
+    }
+}
+
+export function getPortfolioData(timeFrame: string) {
+    switch (timeFrame) {
+        case '24H':
+            return [65, 59, 80, 81, 56];
+        case '1W':
+            return [65, 59, 80, 81, 56, 55, 40];
+        case '1M':
+            return [65, 59, 80, 81];
+        case '1Y':
+            return [65, 59, 80, 81, 56, 55, 40, 70, 80, 90, 100, 110];
+        case 'ALL':
+            return [65, 59, 80, 81, 56, 55];
+        default:
+            return [];
+    }
+}
--- app/ui/dashboard/portfolio-performance.tsx
+++ app/ui/dashboard/portfolio-performance.tsx
@@ -1,7 +1,87 @@
-export default function PortfolioPerformance(){
+'use client'
+import { useState } from 'react';
+import { Line } from 'react-chartjs-2';
+import { Chart, LineElement, PointElement, LinearScale, Title, Tooltip, Legend, CategoryScale } from 'chart.js';
+import { getLabels, getPortfolioData } from '@/app/lib/utils';
+
+Chart.register(LineElement, PointElement, LinearScale, Title, Tooltip, Legend, CategoryScale);
+
+export default function PortfolioPerformance() {
+    const [timeFrame, setTimeFrame] = useState('1M');
+
+    const data = {
+        labels: getLabels(timeFrame),
+        datasets: [
+            {
+                label: 'Portfolio Value',
+                data: getPortfolioData(timeFrame),
+                fill: false,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: '#F8BE4F',
+                tension: 0.4
+            }
+        ]
+    };
+
+    const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Time'
+                }
+            },
+            y: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Value'
+                }
+            }
+        }
+    };
+
     return (
-        <div className="border border-white w-full rounded-md h-64">
-            <p>Portfolio Chart</p>
+        <div className="w-full rounded-md h-64 p-5 mb-10">
+            <div className='flex justify-between items-center'>
+                <p className="text-left font-semibold mb-4">Portfolio Performance</p>
+                <div className="flex justify-between mb-4 gap-x-2">
+                    <button
+                        onClick={() => setTimeFrame('24H')}
+                        className={`px-5 py-[2px] rounded-[22px] border border-[var(--lume-yellow)] text-sm font-semibold ${timeFrame === '24H' ? 'bg-[var(--lume-yellow)] text-black' : 'bg-transparent text-white'}`}
+                    >
+                        24H
+                    </button>
+                    <button
+                        onClick={() => setTimeFrame('1W')}
+                        className={`px-5 py-[2px] rounded-[22px] border border-[var(--lume-yellow)] text-sm font-semibold ${timeFrame === '1W' ? 'bg-[var(--lume-yellow)] text-black' : 'bg-transparent text-white'}`}
+                    >
+                        1W
+                    </button>
+                    <button
+                        onClick={() => setTimeFrame('1M')}
+                        className={`px-5 py-[2px]  rounded-[22px] border border-[var(--lume-yellow)] text-sm font-semibold ${timeFrame === '1M' ? 'bg-[var(--lume-yellow)] text-black' : 'bg-transparent text-white'}`}
+                    >
+                        1M
+                    </button>
+                    <button
+                        onClick={() => setTimeFrame('1Y')}
+                        className={`px-5 py-[2px]  rounded-[22px] border border-[var(--lume-yellow)] text-sm font-semibold ${timeFrame === '1Y' ? 'bg-[var(--lume-yellow)] text-black' : 'bg-transparent text-white'}`}
+                    >
+                        1Y
+                    </button>
+                    <button
+                        onClick={() => setTimeFrame('ALL')}
+                        className={`px-5 py-[2px]  rounded-[22px] border border-[var(--lume-yellow)] text-sm font-semibold ${timeFrame === 'ALL' ? 'bg-[var(--lume-yellow)] text-black' : 'bg-transparent text-white'}`}
+                    >
+                        ALL
+                    </button>
+            </div>
+            </div>
+            <Line data={data} options={options} />
         </div>
-    )
+    );
 }
