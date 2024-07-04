3d459895e28193ec0b4cd8880bc9fe88d59e2d37
Deji Martins <dejimartins99@gmail.com>
2024-07-04 20:44:48 +0100

    margin for mobile changed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,7 +8,7 @@ import Purchase, { PurchaseButtons } from "../ui/dashboard/purchase";
 export default function Page() {
     return (
         <div className="flex overflow-hidden h-[80vh] md:h-full">
-            <div className="flex-grow mr-5 overflow-auto">
+            <div className="flex-grow overflow-auto md:mr-5">
                 <Card />
 
                 <div className="grid-cols-2 my-3 gap-3 min-h-40 pt-5 hidden md:grid">
