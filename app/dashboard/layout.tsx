1ba452ea067f763c43bc2b240f698f2bcec8a9c7
Deji Martins <dejimartins99@gmail.com>
2024-07-04 20:08:32 +0100

    responsiveness

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,27 +1,3 @@
-// 'use client'
-// import SideNav from "../ui/dashboard/sidenav";
-// import TopNav from "../ui/dashboard/topnav";
-// import { dmSans } from "../ui/fonts";
-// import { useState } from 'react';
-
-// export default function Layout({ children }: { children: React.ReactNode }) {
-//     const [isCollapsed, setIsCollapsed] = useState(false);
-
-//     return (
-//         <div className={`${dmSans.className} md:flex bg-[#0A0A0A] h-full md:h-screen md:flex-row md:overflow-y-auto`}>
-//             <div className={`transition-width duration-300 ${isCollapsed ? 'w-16' : 'w-64'} flex-none hidden md:block`}>
-//                 <SideNav isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
-//             </div>
-//             <div className="md:flex-grow overflow-auto">
-//                 <TopNav />
-//                 <div className="p-6 md:px-5">
-//                     {children}
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
 'use client'
 import SideNav from "../ui/dashboard/sidenav";
 import TopNav from "../ui/dashboard/topnav";
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,35 +1,3 @@
-// import Adverts from "../ui/dashboard/advertisments";
-// import Card from "../ui/dashboard/asset-cards";
-// import LatestActivities from "../ui/dashboard/latest-activities";
-// import PortfolioPerformance from "../ui/dashboard/portfolio-performance";
-// import Profile from "../ui/dashboard/profile-card";
-// import Purchase, { PurchaseButtons } from "../ui/dashboard/purchase";
-
-// export default function Page() {
-//     return (
-//         <div className="flex overflow-hidden">
-//             <div className="flex-grow mr-5 overflow-x-auto h-full">
-//                 <Card />
-
-//                 <div className="grid-cols-2 my-3 gap-3 min-h-40 pt-5 hidden md:grid">
-//                     <LatestActivities />
-//                     <Adverts />
-//                 </div>
-
-//                 <div>
-//                     <PurchaseButtons />
-//                     <PortfolioPerformance />
-//                 </div>
-//             </div>
-
-//             <div className="w-80 h-full hidden md:block overflow-auto">
-//                 <Profile />
-//                 <Purchase />
-//             </div>
-//         </div>
-//     )
-// }
-
 import Adverts from "../ui/dashboard/advertisments";
 import Card from "../ui/dashboard/asset-cards";
 import LatestActivities from "../ui/dashboard/latest-activities";
